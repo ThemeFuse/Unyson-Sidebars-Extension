6acee191830eee38fd20c86b736feffc246b4201
themefuse <hello@themefuse.com>
2014-11-27 09:37:19 +0200

    first commit

---
+++ manifest.php
@@ -0,0 +1,13 @@
+<?php if ( ! defined( 'FW' ) ) {
+	die( 'Forbidden' );
+}
+
+$manifest = array();
+
+$manifest['name']        = __( 'Sidebars', 'fw' );
+$manifest['description'] = __( 'Brings another layer of customization freedom to your website by letting you add more than one sidebar to a page, or different sidebars on different pages.', 'fw' );
+$manifest['version'] = '1.0.0';
+$manifest['display'] = true;
+$manifest['standalone']   = true;
+
+$manifest['github_update'] = 'ThemeFuse/Unyson-Sidebars-Extension';

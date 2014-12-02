2205dd3dfd18582b7c243cf8d9f37ff78c732df6
Dima <player0k@mail.ru>
2014-12-02 16:38:46 +0200

    version changed

--- manifest.php
+++ manifest.php
@@ -6,7 +6,7 @@ $manifest = array();
 
 $manifest['name']        = __( 'Sidebars', 'fw' );
 $manifest['description'] = __( 'Brings another layer of customization freedom to your website by letting you add more than one sidebar to a page, or different sidebars on different pages.', 'fw' );
-$manifest['version'] = '1.0.0';
+$manifest['version'] = '1.0.1';
 $manifest['display'] = true;
 $manifest['standalone']   = true;
 

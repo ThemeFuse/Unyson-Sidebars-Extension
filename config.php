30242f949aa8ef015639bdc4acf4a1b862fae747
moldcraft <moldcraft@email.com>
2015-03-20 15:24:26 +0200

    Version 1.0.3

--- config.php
+++ config.php
@@ -32,6 +32,9 @@ $cfg['dynamic_sidebar_args'] = array(
 	'before_title'  => '<h1 class="widget-title">',
 	'after_title'   => '</h1>',
 );
-
-$cfg['show_in_post_types'] = false;
 */
+
+/**
+ * Render sidebar metabox in post types.
+ */
+$cfg['show_in_post_types'] = false;

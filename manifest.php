67f41bba54dbc38f84b80cc33bb6abdb25e57695
moldcraft <moldcraft@email.com>
2015-06-17 16:00:49 +0300

    Version 1.0.5

--- manifest.php
+++ manifest.php
@@ -5,8 +5,12 @@
 $manifest = array();
 
 $manifest['name']         = __( 'Sidebars', 'fw' );
-$manifest['description']  = __( 'Brings a new layer of customization freedom to your website by letting you add more than one sidebar to a page, or different sidebars on different pages.', 'fw' );
-$manifest['version']      = '1.0.4';
+$manifest['description']  = __(
+	'Brings a new layer of customization freedom to your website by letting you add more than one sidebar to a page,'
+	.' or different sidebars on different pages.',
+	'fw'
+);
+$manifest['version']      = '1.0.5';
 $manifest['display']      = true;
 $manifest['standalone']   = true;
 $manifest['requirements'] = array(

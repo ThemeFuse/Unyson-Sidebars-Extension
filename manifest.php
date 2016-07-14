997b38170c41494c2b4e457e564418293a5dd35d
moldcraft <moldcraft@email.com>
2016-07-14 13:27:31 +0300

    Version 1.0.9

--- manifest.php
+++ manifest.php
@@ -10,7 +10,7 @@ $manifest['description']  = __(
 	.' or different sidebars on different pages.',
 	'fw'
 );
-$manifest['version']      = '1.0.8';
+$manifest['version']      = '1.0.9';
 $manifest['display']      = true;
 $manifest['standalone']   = true;
 $manifest['requirements'] = array(

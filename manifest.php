5d2fcca6097ccb87b004de5993f9e2f15987a1f0
Gheorghe <pisarenco.gh@gmail.com>
2017-06-23 11:05:47 +0300

    Version 1.0.17

--- manifest.php
+++ manifest.php
@@ -10,7 +10,7 @@ $manifest['description']  = __(
 	.' or different sidebars on different pages.',
 	'fw'
 );
-$manifest['version']      = '1.0.16';
+$manifest['version']      = '1.0.17';
 $manifest['display']      = true;
 $manifest['standalone']   = true;
 $manifest['requirements'] = array(
